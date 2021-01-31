f07e4332ee4541f8c4d9bb967608a8ce6eb9fc15
Pavithra MH <pavithra.mh@talentbridge.co.in>
2021-01-31 23:30:58 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by TalentBridge Mac - 1 on 31/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
